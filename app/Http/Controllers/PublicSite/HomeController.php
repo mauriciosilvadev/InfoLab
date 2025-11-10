deb2f9626e4005240a260f049c7470b26fa9a030
Mauricio Silva <mauricio.s.dev@gmail.com>
2025-11-10 18:08:07 -0300

    fix(HomeController): add type hint for FilesystemAdapter in HomeController
    - Introduce a type hint for the FilesystemAdapter in the HomeController to improve code clarity and maintainability.

--- app/Http/Controllers/PublicSite/HomeController.php
+++ app/Http/Controllers/PublicSite/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends Controller
 {
     public function __invoke(): View
     {
+        /** @var \Illuminate\Filesystem\FilesystemAdapter $disk */
         $disk = Storage::disk('public');
 
         $localDisk = Storage::disk('local');
